0f01f7b1159034de291cbbac6fff412a9e12713b
QA17 <QA17@QAPLANET-PC>
2020-02-25 12:47:33 +0530

    added class in github package

--- Advanced_Topics/src/gitHub/practice.java
+++ Advanced_Topics/src/gitHub/practice.java
@@ -1,9 +1,18 @@
 package gitHub;
 
+import java.util.ArrayList;
+
 public class practice {
 
 	public static void main(String[] args) {
+		
 		System.out.println("github demo");
+		ArrayList a=new ArrayList<>();
+		a.add(10);
+		a.add("reddy");
+		System.out.println(a.get(0));
+		System.out.println(a.get(1));
+		System.out.println(a.size());
 
 	}
 
